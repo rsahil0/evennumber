b01ed8919afccefa263ff659b3d6c4dca082f8a0
rsahil0 <90460263+rsahil0@users.noreply.github.com>
2021-11-22 11:05:15 +0530

    Add files via upload

---
+++ evenNumber.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+    class Addition {
+        public static void main(String[] args)
+        {
+            // create a list of integers
+            List<Integer> list = new ArrayList<Integer>();
+
+            // add elements to the list
+            list.add(1);
+            list.add(5);
+            list.add(6);
+            list.add(7);
+            list.add(8);
+            list.add(9);
+            list.add(10);
+
+            System.out.println(sum(list));
+        }
+
+        public static int sum(List<Integer> list)
+        {
+            // create a stream of integers
+            // filter the stream
+            // add the integers
+            return list.stream()
+                    .filter(i -> i > 5)
+                    .mapToInt(i -> i)
+                    .sum();
+        }
+    }
+
